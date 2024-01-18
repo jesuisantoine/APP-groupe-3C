57d8d75cf5cc5e29539d5ec5337735c603870bca
matrixc11 <147148886+matrixc11@users.noreply.github.com>
2024-01-18 12:16:49 +0100

    Create Login.php

---
+++ APP-G3B-2023/public_html/Login.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>login</title>
+        <link href="form_css.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+
+        <div class="login-box">
+            <h2>Se connecter</h2>
+            <form>
+                <div class="user-box">
+                    <input type="text" name="" required="">
+                    <label>email</label>
+                </div>
+                <div class="user-box">
+                    <input type="password" name="" required="">
+                    <label>mot de passe</label>
+                </div>
+              
+                    <input type="submit" value="connexion" class="connect">
+                       
+            </form>
+            
+                <a href="re_form.php"><button class="regi">Créer un compte</button></a>
+        </div>
+    </body>
+</html>
